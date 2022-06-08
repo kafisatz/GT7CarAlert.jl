2cf4da387fa868e94d40c816b490856d5bde110c
kafisatz <github@diro.ch>
2022-06-08 19:21:29 +0200

    edt

--- src/main_script.jl
+++ src/main_script.jl
@@ -5,7 +5,7 @@ Pkg.add(url=raw"https://github.com/kafisatz/GT7CarAlert.jl")
 =#
 
 #run the next line in order to trigger a package update
-#using Pkg; Pkg.update("GT7CarAlert.jl")
+#using Pkg; Pkg.update()
 
 @assert VERSION >= v"1.6"
 using GT7CarAlert
