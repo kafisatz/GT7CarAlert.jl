42da564604d291f4e4342bfb44d448233ff02efe
Bernhard König <github@diro.ch>
2022-06-08 17:59:41 +0200

    edt

--- src/main_script.jl
+++ src/main_script.jl
@@ -7,22 +7,39 @@ using DelimitedFiles
 using JSON3
 using Pushover
 
-#initialization
+##############################################
+#Configuration
+##############################################
+#EDIT these paths as needed (and delete path the default path definitions below)
+#examples
+pushover_config_file = raw"C:\some folder\some_file.txt"
+car_list_file = raw"C:\some folder\some_file.txt"
+
 pushover_config_file = joinpath(ENV["HOMEPATH"],".pushover","pushover_config.txt")
+
+car_list_file = normpath(joinpath(pathof(GT7CarAlert),"..","..","gt7_missing_cars.csv"))
+
+number_of_seconds_to_sleep = 120
+
+##############################################
+#Initialization of script settings
+##############################################
+@assert isfile(car_list_file)
 @assert isfile(pushover_config_file)
+
 pocreds = get_pushover_creds(pushover_config_file)
 @assert isa(pocreds,Dict)
 @assert haskey(pocreds,"USER_KEY")
 @assert haskey(pocreds,"API_TOKEN")
+
 return_value_pushover_test = send_notification(pocreds,"Pushover test message from GT7CarAlert.jl script.")
 @show return_value_pushover_test
     
-car_list_file = normpath(joinpath(pathof(GT7CarAlert),"..","..","gt7_missing_cars.csv"))
-@assert isfile(car_list_file)
-
-number_of_seconds_to_sleep = 120
 @assert number_of_seconds_to_sleep>0
 
+##############################################
+#Start infinite loop
+##############################################
 while true
     try
         main_loop_function(pocreds,car_list_file)
