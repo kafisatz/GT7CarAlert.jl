d7fd89699b4e34c3efc64944457cc4ef0721f2b1
kafisatz <github@diro.ch>
2022-06-08 18:25:01 +0200

    edits

--- src/GT7CarAlert.jl
+++ src/GT7CarAlert.jl
@@ -133,4 +133,64 @@ function main_loop_function(pocreds,car_list_file)
     end
 end
 
+export main_script_with_init 
+function main_script_with_init()
+#=
+#activate package (should only be needed when invoking batch file)
+using Pkg
+Pkg.activate(".")
+
+using GT7CarAlert
+using DelimitedFiles
+using JSON3
+using Pushover
+=#
+
+##############################################
+#Configuration
+##############################################
+pushover_config_file = joinpath(ENV["HOMEPATH"],".pushover","pushover_config.txt")
+if haskey(ENV,"PUSHOVER_CONFIG_GT7") && isfile(ENV["PUSHOVER_CONFIG_GT7"])
+    pushover_config_file = ENV["PUSHOVER_CONFIG_GT7"]
+end
+
+car_list_file = normpath(joinpath(pathof(GT7CarAlert),"..","..","gt7_missing_cars.csv"))
+if haskey(ENV,"CAR_LIST_GT7") && isfile(ENV["CAR_LIST_GT7"])
+    car_list_file = ENV["CAR_LIST_GT7"]
+end
+
+number_of_seconds_to_sleep = 120
+
+##############################################
+#Initialization of script settings
+##############################################
+@assert isfile(car_list_file)
+@assert isfile(pushover_config_file)
+
+pocreds = get_pushover_creds(pushover_config_file)
+@assert isa(pocreds,Dict)
+@assert haskey(pocreds,"USER_KEY")
+@assert haskey(pocreds,"API_TOKEN")
+
+return_value_pushover_test = send_notification(pocreds,"Pushover test message from GT7CarAlert.jl script.")
+@show return_value_pushover_test
+    
+@assert number_of_seconds_to_sleep>0
+
+##############################################
+#Start infinite loop
+##############################################
+while true
+    try
+        main_loop_function(pocreds,car_list_file)
+    catch er         
+        @show er
+        @warn("GT7CarAlert.jl script ran into an error (see message above this line)!\r\nThe script will retry in $(number_of_seconds_to_sleep) seconds:")
+    end
+    sleep(number_of_seconds_to_sleep)
+end
+
+return nothing
+end
+
 end # module
--- src/main_script.jl
+++ src/main_script.jl
@@ -1,52 +1,13 @@
-#activate package (should only be needed when invoking batch file)
+@assert VERSION >= v"1.6"
 using Pkg
-Pkg.activate(".")
+Pkg.add(url=raw"https://github.com/kafisatz/GT7CarAlert.jl")
 
 using GT7CarAlert
-using DelimitedFiles
-using JSON3
-using Pushover
 
-##############################################
-#Configuration
-##############################################
-#EDIT these paths as needed (and delete path the default path definitions below)
-#examples
-pushover_config_file = raw"C:\some folder\some_file.txt"
-car_list_file = raw"C:\some folder\some_file.txt"
+ENV["PUSHOVER_CONFIG_GT7"] = raw"C:\Users\bernhard\.pushover\pushover_config.txt"
+ENV["CAR_LIST_GT7"] = normpath(joinpath(pathof(GT7CarAlert),"..","..","gt7_missing_cars.csv"))
 
-pushover_config_file = joinpath(ENV["HOMEPATH"],".pushover","pushover_config.txt")
+@assert isfile(ENV["PUSHOVER_CONFIG_GT7"])
+@assert isfile(ENV["CAR_LIST_GT7"])
 
-car_list_file = normpath(joinpath(pathof(GT7CarAlert),"..","..","gt7_missing_cars.csv"))
-
-number_of_seconds_to_sleep = 120
-
-##############################################
-#Initialization of script settings
-##############################################
-@assert isfile(car_list_file)
-@assert isfile(pushover_config_file)
-
-pocreds = get_pushover_creds(pushover_config_file)
-@assert isa(pocreds,Dict)
-@assert haskey(pocreds,"USER_KEY")
-@assert haskey(pocreds,"API_TOKEN")
-
-return_value_pushover_test = send_notification(pocreds,"Pushover test message from GT7CarAlert.jl script.")
-@show return_value_pushover_test
-    
-@assert number_of_seconds_to_sleep>0
-
-##############################################
-#Start infinite loop
-##############################################
-while true
-    try
-        main_loop_function(pocreds,car_list_file)
-    catch er         
-        @show er
-        @warn("GT7CarAlert.jl script ran into an error (see message above this line)!\r\nThe script will retry in $(number_of_seconds_to_sleep) seconds:")
-    end
-    sleep(number_of_seconds_to_sleep)
-end
-
+GT7CarAlert.main_script_with_init()
