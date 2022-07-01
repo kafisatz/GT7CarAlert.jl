a169c3e422e7f85a9cf904d7a3dc62c0fa1a99fd
Bernhard König <github@diro.ch>
2022-07-01 02:38:57 +0200

    edt0

--- src/jot_script.jl
+++ src/jot_script.jl
@@ -5,6 +5,7 @@ using GT7CarAlert
 using Jot
 
 responder = get_responder(GT7CarAlert, :response_func, Vector{String})
+#dependencies=["GT7CarAlert"]
 #responder = get_responder("/path/to/project", :response_func, String)
 #responder = get_responder("https://github.com/kafisatz/GT7CarAlert.jl", :response_func, Vector)
 
@@ -12,6 +13,7 @@ use_pc = true
 use_pc = false
 #create_lambda_components
 local_image = create_local_image(responder,julia_base_version="1.6.6",package_compile = use_pc)
+@edit create_local_image(responder,julia_base_version="1.6.6",package_compile = use_pc)
 
 run_test(local_imagePC, "test", "test Responded")
 
