5e4b10514505d3d2b70d295e70eea1cbbcc330d4
Bernhard König <github@diro.ch>
2022-07-01 02:56:47 +0200

    edt0

--- src/jot_script.jl
+++ src/jot_script.jl
@@ -8,7 +8,10 @@ car_list_file = normpath(joinpath(pathof(GT7CarAlert),"..","..","gt7_missing_car
 cars_want = read_carlist(car_list_file)
 test_inputv = ["mihi","muhi x"]
 test_inputv2 = cars_want = read_carlist(car_list_file)
-GT7CarAlert.response_func(test_inputv2)
+@assert (true,String[]) == GT7CarAlert.response_func(test_inputv)
+#the next asswert will depend on the cars actually listed at the current time!
+@assert (true,["Ferrari F40 '92", "Jaguar XJ13 '66"]) == GT7CarAlert.response_func(test_inputv2)
+
 
 responder = get_responder(GT7CarAlert, :response_func, Vector{String})
 #dependencies=["GT7CarAlert"]
@@ -23,7 +26,9 @@ local_image = create_local_image(responder,julia_base_version="1.6.6",package_co
 
 run_test(local_image, ["test","mihi x"], "test Responded")
 
-remote_image = push_to_ecr!(local_imagePC)
+show_lambdas()
+
+remote_image = push_to_ecr!(local_image)
 lambda = create_lambda_function(remote_image)
 
 
